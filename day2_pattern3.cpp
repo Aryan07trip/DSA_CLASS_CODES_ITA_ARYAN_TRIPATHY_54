38991a0222690f271412fe540a261fa8c74f63f0
Aryan07trip <119985843+Aryan07trip@users.noreply.github.com>
2023-09-17 19:12:02 +0530

    Create day2_pattern3.cpp

---
+++ day2_pattern3.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char n;
+    cin>>n;
+    for(char c='A';c<=n;c++)
+    {
+        for(char c1='A';c1<=c;c1++)
+        {
+          cout<<c;
+        }
+        cout<<endl;
+    }
+}
