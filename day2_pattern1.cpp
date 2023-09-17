3d8c1d13c58cb463c81cfe1d90af31ab714063fe
Aryan07trip <119985843+Aryan07trip@users.noreply.github.com>
2023-09-17 19:10:19 +0530

    Create day2_pattern1.cpp

---
+++ day2_pattern1.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int k;
+    for(int i=1;i<=4;i++)
+    {
+        k=i;
+        for(int j=1;j<=i;j++)
+        {
+          cout<<k;
+          k++;
+        }
+        cout<<endl;
+    }
+}
